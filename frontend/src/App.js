09e5f1cc527c771395e6cad21c441da017d4615b
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2021-12-17 00:18:00 -0300

    Instalação do primeflex , primeicons e primereact
    instalação de suas dependências.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import Home from "./pages/Home";
 
+import {PrimeReactCSS} from "primereact/resources/themes/saga-green/theme.css";
+import {PrimeReactMinCsss} from "primereact/resources/primereact.min.css";
+import {Icons} from "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
+
 function App() {
   return (
     <Home></Home>
