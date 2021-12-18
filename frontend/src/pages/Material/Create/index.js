1f5c7c773b7561bc3a4a22ba9a583e7c03db52de
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2021-12-18 01:54:13 -0300

    ajuste materiais create (Ajuste ainda são necessários)
    add componente CascadeSelect em materiais create

--- frontend/src/pages/Material/Create/index.js
+++ frontend/src/pages/Material/Create/index.js
@@ -1,11 +1,95 @@
 import React from 'react';
 import './styles.css'
+import { useState } from 'react';
 
 import { InputText } from 'primereact/inputtext';
-//import { CascadeSelect } from 'primereact/cascadeselect';
-
+import { CascadeSelect } from 'primereact/cascadeselect';
 
 export default function Create(){
+
+    const countries = [
+        {
+            name: 'Australia',
+            code: 'AU',
+            states: [
+                {
+                    name: 'New South Wales',
+                    cities: [
+                        {cname: 'Sydney', code: 'A-SY'},
+                        {cname: 'Newcastle', code: 'A-NE'},
+                        {cname: 'Wollongong', code: 'A-WO'}
+                    ]
+                },
+                {
+                    name: 'Queensland',
+                    cities: [
+                        {cname: 'Brisbane', code: 'A-BR'},
+                        {cname: 'Townsville', code: 'A-TO'}
+                    ]
+                },
+    
+            ]
+        },
+        {
+            name: 'Canada',
+            code: 'CA',
+            states: [
+                {
+                    name: 'Quebec',
+                    cities: [
+                        {cname: 'Montreal', code: 'C-MO'},
+                        {cname: 'Quebec City', code: 'C-QU'}
+                    ]
+                },
+                {
+                    name: 'Ontario',
+                    cities: [
+                        {cname: 'Ottawa', code: 'C-OT'},
+                        {cname: 'Toronto', code: 'C-TO'}
+                    ]
+                },
+    
+            ]
+        },
+        {
+            name: 'United States',
+            code: 'US',
+            states: [
+                {
+                    name: 'California',
+                    cities: [
+                        {cname: 'Los Angeles', code: 'US-LA'},
+                        {cname: 'San Diego', code: 'US-SD'},
+                        {cname: 'San Francisco', code: 'US-SF'}
+                    ]
+                },
+                {
+                    name: 'Florida',
+                    cities: [
+                        {cname: 'Jacksonville', code: 'US-JA'},
+                        {cname: 'Miami', code: 'US-MI'},
+                        {cname: 'Tampa', code: 'US-TA'},
+                        {cname: 'Orlando', code: 'US-OR'}
+                    ]
+                },
+                {
+                    name: 'Texas',
+                    cities: [
+                        {cname: 'Austin', code: 'US-AU'},
+                        {cname: 'Dallas', code: 'US-DA'},
+                        {cname: 'Houston', code: 'US-HO'}
+                    ]
+                }
+            ]
+        }
+    ]
+    
+    const [selectedCity, setSelectedCity1] = useState('Canada');
+    
+    function mudarEstado(){
+        setSelectedCity1('United States');
+    }
+
     return (
         //<InputText className='ccc' value={value} onChange={(e) => setValue(e.target.value)} />
         
@@ -19,7 +103,8 @@ export default function Create(){
                                                     
             <h5>CATEGORIA</h5>
             
-            <InputText className='entradaeSPAÇOCATEGORIA' ></InputText>
+            <CascadeSelect  value={selectedCity} options={countries}  optionLabel={"cname"} optionGroupLabel={"name"} optionGroupChildren={['states', 'cities']}
+                style={{minWidth: '14rem'}} placeholder={"Select a City"} onChange={event => setSelectedCity1(event.value)}/>
                                 <button className = "btnNovaCategoria" type = "submit">NOVA CATEGORIA</button>
 
         <h5>ARQUIVO</h5>
