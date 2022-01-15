e6561ea7e8a6361308f7dbf7b5c1fdbea4b4f01d
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-01-15 01:56:19 -0300

    criação do projeto Spring boot
    inicialização do novo backend e startes iniciais do Spring

---
+++ backend/HelpMind/src/main/java/com/helpmind/HelpMindApplication.java
@@ -0,0 +1,13 @@
+package com.helpmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMindApplication.class, args);
+	}
+
+}
