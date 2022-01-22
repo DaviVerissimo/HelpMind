b731b8fda2604b4b04870a17f2dad342a0f3385a
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-01-22 20:03:19 -0300

    tabela dos reportes (Não foi possivel exibir diretamente na tabela)
    conteúdo sendo exibido em tela somente para feedback visual.

--- frontend/src/pages/ADM/ListaDeReportes/index.js
+++ frontend/src/pages/ADM/ListaDeReportes/index.js
@@ -1,36 +1,62 @@
 
 
 import React, {useState, useEffect} from 'react'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import ReporteService from '../../../services/ReporteService';
 
 export default function ListaDeReportes() {
 
     const [reportes, setReportes] = useState([])
-
+    
     useEffect(() => {
         getReportes()
+        //console.log(reportes);
     }, [])
 
     const getReportes = () => {
 
         ReporteService.getReporte().then((response) => {
             setReportes(response.data)
-            console.log(response.data);
+           // console.log(response.data);
         });
     };
 
-    return (
-        <div className = "container">
+    {
+        reportes.map((reporte, key) => {
+            console.log({reportes});
+        })
+    }
+    return ( 
+        <div> 
+            <div className="card">
+                <DataTable value={reportes} responsiveLayout="scroll">
+                    <Column field="Discente" header="Discente" ></Column>
+                    <Column field="ID" header="ID"></Column>
+                    <Column field="Curso" header="Curso"></Column>
+                    <Column field="Campus" header="Campus"></Column>
+                    <Column field="Periodo" header="Periodo"></Column>
+                    <Column field="Ten de Suicidio" header="Ten de Suicidio"></Column>
+                    <Column field="Descrisão" header="Descrisão"></Column>
+                </DataTable>
+            </div>
+
+            <div>
+            <div className = "container">
             
             <h1 className = "text-center"> Reporte List</h1>
 
             <table className = "table table-striped">
                 <thead>
                     <tr>
-                        <th> Reporte Id</th>
-                        <th> Reporte First Name</th>
-                        <th> Reporte Last</th>
-                        <th> Reporte Email</th>
+                        <th>  Id</th>
+                        <th>  Discente</th>
+                        <th>  Curso</th>
+                        <th>  Periodo</th>
+                        <th>  Campus</th>
+                        <th>  Periodo</th>
+                        <th>  Suicidio</th>
+                        <th>  Descrisão</th>
                     </tr>
 
                 </thead>
@@ -43,6 +69,9 @@ export default function ListaDeReportes() {
                                     <td> {reporte.discente }</td>
                                     <td> {reporte.curso }</td>    
                                     <td> {reporte.periodo }</td>
+                                    <td> {reporte.campus }</td>
+                                    <td> {reporte.tentativaDeSuicidio }</td>
+                                    <td> {reporte.descrisao }</td>
 
                                 </tr>
 
@@ -53,6 +82,10 @@ export default function ListaDeReportes() {
 
 
             </table>
+
+        </div>
+
+            </div>
 
         </div>
     )
