a37195144c917f817a175819b51f1eac140c9fe2
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-03-14 22:05:03 -0300

    refatoração de código Materiais CRUD

--- frontend/src/pages/Material/Crud/index.js
+++ frontend/src/pages/Material/Crud/index.js
@@ -10,52 +10,47 @@ import { Button } from "primereact/button";
 
 export default function Crud() {
 
-    const cities = [
+    const categorias = [
         { name: 'Depresão', code: 'NY' },
         { name: 'Ansiedade', code: 'RM' },
         { name: 'Suicidio', code: 'LDN' },
         { name: 'Outubro Rosa', code: 'IST' },
         { name: 'Biporalidade', code: 'PRS' }
     ];
-
     const [city, setCity] = useState('Ansiedade');
-
-    function mudarEstado() {
-        setCity('United States');
-    }
 
     return (
 
         <div> <ToobarADM></ToobarADM>
-
             <Card title="GERENCIA DE MATERIAIS">
             </Card>
             <div>
-                <h5>CATEGORIA</h5>
-                <Dropdown optionLabel="name" value={city} options={cities} onChange={(e) => setCity(e.value)} placeholder="TODAS" />
+                <Card subTitle='CATEGORIA' >
+                    <Dropdown optionLabel="name" value={city} options={categorias} onChange={(e) => setCity(e.value)} placeholder="TODAS" />
+                </Card>
                 <Card>
-                <div className='' style={{ height: '100%' }}  >
+                    {/* Realizar mapeamento com map a partir de um json e popular a tabela e add as devidas colunas */}
+                    <div className='' style={{ height: '100%' }}  >
                         <div className="card">
-                            <DataTable value={cities} responsiveLayout="scroll">
-                                <Column field="discente" header="Discente" ></Column>
-                                <Column field="id" header="ID"></Column>
+                            <DataTable value={categorias} responsiveLayout="scroll">
+                                <Column field="discente" header="Titulo" ></Column>
+                                {/* <Column field="id" header="ID"></Column>
                                 <Column field="curso" header="Curso"></Column>
                                 <Column field="campus" header="Campus"></Column>
                                 <Column field="periodo" header="Periodo"></Column>
                                 <Column field="tentativaDeSuicidio" header="Ten de Suicidio"></Column>
-                                <Column field="descrisao" header="Descrisão"></Column>
+                                <Column field="descrisao" header="Descrisão"></Column> */}
                                 <Column field="" header="Açoes"
                                     body={
                                         <div>
                                             <Card>
                                                 <div>
-                                                <Button className="p-mb-5  p-mr-3 p-col-0" label="" icon="pi pi-file" />
-                                                <Button className="p-mb-5  p-mr-3 p-col-0" label="" icon="pi pi-pencil" />
-                                                <Button className="p-mb-0  p-mr-3 p-col-0 p-button-danger" label="" icon="pi pi-trash" />
+                                                    <Button className="p-mb-5  p-mr-3 p-col-0" label="" icon="pi pi-file" />
+                                                    <Button className="p-mb-5  p-mr-3 p-col-0" label="" icon="pi pi-pencil" />
+                                                    <Button className="p-mb-0  p-mr-3 p-col-0 p-button-danger" label="" icon="pi pi-trash" />
                                                 </div>
                                             </Card>
                                         </div>
-
                                     }
                                 ></Column>
                             </DataTable>
@@ -63,7 +58,6 @@ export default function Crud() {
                     </div>
                 </Card>
             </div>
-
         </div>
 
     );
