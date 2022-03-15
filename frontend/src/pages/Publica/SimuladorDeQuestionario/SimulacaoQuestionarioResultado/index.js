42b9ba5f9aac74023abce5226c5839500383cc5d
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-03-14 22:40:35 -0300

    refatoração de código Simulação de questionário de beck

--- frontend/src/pages/Publica/SimuladorDeQuestionario/SimulacaoQuestionarioResultado/index.js
+++ frontend/src/pages/Publica/SimuladorDeQuestionario/SimulacaoQuestionarioResultado/index.js
@@ -1,7 +1,6 @@
 import { Card } from 'primereact/card';
 import React from 'react';
 import { Button } from 'primereact/button';
-//import './styles.css'
 
 import ToobarPublica from '../../ToobarPublica/index';
 
@@ -10,23 +9,20 @@ export default function SimulacaoQuestionarioResultado() {
     return (
         <div>
             <ToobarPublica></ToobarPublica>
-            <div className="quemSomos-container p-grid p-justify-center p-align-center p-component  p-dir-col " style={{ height: '100%' }} >
+            <div className="simulacao-container p-grid p-justify-center p-align-center p-component  p-dir-col " style={{ height: '100%' }} >
                 <Card title="RESULTADO"></Card>
-
                 <div className="home-ladoEsquerdo p-col-4 p-mt-3" >
                     <Card
                         subTitle="PONTOS" >
                         <h1>26</h1>
                     </Card>
                 </div>
-
-                <div className="home-ladoEsquerdo p-col-4 p-mt-3" >
+                <div className=" p-col-4 p-mt-3" >
                     <Card subTitle="RESULTADO" >
                         <h1>Depresão moderada</h1>
                     </Card>
                 </div>
-
-                <div className="home-ladoEsquerdo p-col-4 p-mt-3" >
+                <div className=" p-col-4 p-mt-3" >
                     <Card title="Recomendamos que realize um auto reporte!"
                         subTitle="O iventario de beck considera todos os valores como depresão. Valores NÂO SIGNIFICAM que vc esterja com depressão." >
                         <div className=" " >
@@ -36,11 +32,7 @@ export default function SimulacaoQuestionarioResultado() {
                         </div>
                     </Card>
                 </div>
-
-
-
             </div>
-
         </div>
     );
 }
