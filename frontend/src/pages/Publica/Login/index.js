203fbc68faf0021b75d272b77945f854ebf8ec46
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-04-15 22:17:40 -0300

    Responsividade Tela Login
    Tela Login agora tem um comportamento diferente em telas de Smartphone.

--- frontend/src/pages/Publica/Login/index.js
+++ frontend/src/pages/Publica/Login/index.js
@@ -7,19 +7,34 @@ import logoImg from '../../../assets/logo.png';
 import ToobarPublica from '../ToobarPublica';
 
 export default function Login() {
+
+    var configBotaoSecumdario = "p-mb-3 p-col-4 p-button-secondary";
+    var configBotaoPrimario = "p-mb-3 p-col-4";
+    var sizeCard = "login p-col-6";
+    var espacamento = '10px';
+    var altura = window. screen. height;
+    var largura = window. screen. width;
+    console.log(altura)
+    console.log(largura)
+    if (largura < 640){
+        configBotaoSecumdario = "p-mb-3 p-button-secondary "
+        configBotaoPrimario = "p-mt-3 ";
+        sizeCard = "";
+    }
+
     return (
         <div>
             <ToobarPublica />
             <div className="login-container p-grid p-justify-center p-align-center p-mt-3" style={{ height: '100%' }} >
 
-                <div className="login p-col-6" >
+                <div className={sizeCard} >
                     <Card title="SEJA BEM VINDO ESTUDANTE E COMUNIDADE ACADÊMICA"
                         subTitle="aqui opções de ajuda ao estudante, e auxilio aos profissionais de saúde " >
 
                         <div  >
-                            <Button className="p-mb-3 p-col-4" label="ENTRAR COM E-MAIL ACADEMICO" />
+                            <Button className={configBotaoPrimario} label="ENTRAR COM E-MAIL ACADEMICO" />
                             <h3>ou</h3>
-                            <Button className="p-mb-3 p-col-4 p-button-secondary" label="NÂO POSSUO E-MAIL ACADEMICO" />
+                            <Button className={configBotaoSecumdario} label="NÂO POSSUO E-MAIL ACADEMICO" />
                         </div>
 
                         <Card subTitle='Realize login para acessar todas as opções: ' ></Card>
