feec9fc886e12ccd5584cd0871cdd2e5bdd01a8c
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-05-09 04:18:30 -0300

    Ajuste de layout toobar Discente

--- frontend/src/pages/Discente/ToobarDiscente/index.js
+++ frontend/src/pages/Discente/ToobarDiscente/index.js
@@ -55,15 +55,14 @@ export default function ToobarDiscente() {
                     <img src={logoImg} alt="logo" style={{ height: '3.75em', width: 'auto' }} />
 
                 </div> */}
-                <Button icon={createIcon("pi pi-home")} className="p-button-outlined p-button-lg p-mr-3 p-col " label='Home' onClick={() => { history.push('/') }} />
-                <Button icon={createIcon("pi pi-box")} className="p-button-outlined p-button-lg p-mr-3 p-col  " label='Materiais online' onClick={() => { history.push('/publica/MateriaisOnline') }} />
+                <Button icon={createIcon("pi pi-home")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3" label='Home' onClick={() => { history.push('/') }} />
+                <Button icon={createIcon("pi pi-box")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3 " label='Materiais online' onClick={() => { history.push('/publica/MateriaisOnline') }} />
                 {/* <Button icon={createIcon("pi pi-heart-fill")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Simular Questionario' /> */}
-                <Button icon={createIcon("pi pi-info-circle")} className="p-button-outlined p-button-lg p-mr-3 p-col " label='Quem Somos' onClick={() => { history.push('/publica/QuemSomos') }} />
-                <Button icon={createIcon("pi pi-comments")} className="p-button-outlined p-button-lg p-mr-3 p-col " label='Contatos' />
-                <Button icon={createIcon("pi pi-file-o")} className="p-button-outlined p-button-lg p-mr-3 p-col " label='Inventario' onClick={() => { history.push('/discente/EscolherQuestionariosDiscente') }} />
-                {/* <Button icon={createIcon("pi pi-users")} className="p-button-outlined p-button-lg p-mr-3 p-col" style={{ height: '4.00em', width: 'auto' }} label='Discentes' /> */}
-                <Button icon={createIcon("pi pi-bell")} className="p-button-outlined p-button-lg p-mr-3 p-col  " label='Reportes' onClick={() => { history.push('/discente/Reportes') }} />
-                <Button icon={createIcon("pi pi-user")} className="p-button-outlined p-button-lg p-mr-3 p-col  " label='Discente' onClick={() => { history.push('/discente/Perfil') }} />
+                <Button icon={createIcon("pi pi-info-circle")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3" label='Quem Somos' onClick={() => { history.push('/publica/QuemSomos') }} />
+                <Button icon={createIcon("pi pi-comments")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3" label='Contatos' />
+                <Button icon={createIcon("pi pi-file-o")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3" label='Inventario' onClick={() => { history.push('/discente/EscolherQuestionariosDiscente') }} />
+                <Button icon={createIcon("pi pi-bell")} className="p-button-outlined p-button-lg p-mr-3 p-col p-mt-3 " label='Reportes' onClick={() => { history.push('/discente/Reportes') }} />
+                <Button icon={createIcon("pi pi-user")} className="p-button-outlined p-button-lg p-mr-3 p-col  p-mt-3" label='Discente' onClick={() => { history.push('/discente/Perfil') }} />
             </div>
         </React.Fragment>
     );
