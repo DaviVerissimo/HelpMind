2fa18d92eb8b4dc210c8d1ea694c41a3a15a4f48
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-05-09 03:02:54 -0300

    Ajuste de layout reporte

--- frontend/src/pages/Discente/Reporte/index.js
+++ frontend/src/pages/Discente/Reporte/index.js
@@ -35,8 +35,7 @@ export default function Reporte() {
     var largura = window. screen. width;
     if (largura < 640){
         configBotaoCancel = "p-mb-3 p-button-secondary "
-        configBotaoSalvar = "p-mt-3 ";
-        espacamento = '0px';
+        configBotaoSalvar = "p-mb-3 ";
     }
 
     useEffect(async () => { //cursos
