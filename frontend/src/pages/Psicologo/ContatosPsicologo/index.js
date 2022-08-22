9f3a214ed4ec518fbe301c0caecf7a24f9e16ce6
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2022-08-21 23:46:58 -0300

    Psicologo Telas
    Telas restantes do usuário psicologo.

---
+++ frontend/src/pages/Psicologo/ContatosPsicologo/index.js
@@ -0,0 +1,12 @@
+import ToobarPsicologo from "../ToobarPsicologo";
+import ListarContatos from "../../../Components/listarContatos";
+
+export default function ContatosPsicologo() {
+
+    return (
+        <div>
+            <ToobarPsicologo></ToobarPsicologo>
+            <ListarContatos></ListarContatos>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Psicologo/ToobarPsicologo/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import './styles.css'
+import { Toolbar } from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+import logoImg from './assets/favicon.ico';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import FotoPerfil from '../../FotoPerfil';
+import NomePerfil from '../../NomePerfil';
+
+export default function ToobarPsicologo() {
+
+    var altura = window.screen.height;
+    var largura = window.screen.width;
+    const history = useHistory();
+    const createIcon = (className) => {
+        return <i className={className} style={{ 'fontSize': '2em' }}></i>
+    }
+
+    const rightContents = (
+        <React.Fragment>
+
+            <div className='p-grid p-col' style={{ padding: 'em' }} >
+                <NomePerfil></NomePerfil>
+                <FotoPerfil></FotoPerfil>
+            </div>
+        </React.Fragment>
+    );
+
+    const leftContents = (
+        <React.Fragment>
+
+            <div className='p-grid p-col' style={{ padding: '1em' }}>
+                <div style={{}} className='p-col-1 p-mr-3'  >
+                    <img src={logoImg} alt="logo" style={{ height: '3.75em', width: 'auto' }} />
+
+                </div>
+                <Button icon={createIcon("pi pi-home")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Home' onClick={() => { history.push('/') }} />
+                <Button icon={createIcon("pi pi-box")} className="p-button-outlined p-button-lg  p-mr-3 p-col" label='Materiais online' onClick={() => { history.push('/publica/MateriaisOnline') }} />
+                {/* <Button icon={createIcon("pi pi-heart-fill")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Simular Questionario' /> */}
+                {/* <Button icon={createIcon("pi pi-info-circle")} className="p-button-outlined p-button-lg  p-mr-3 p-col" label='Quem Somos' onClick={() => { history.push('/publica/QuemSomos') }} /> */}
+                <Button icon={createIcon("pi pi-comments")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Contatos' onClick={() => { history.push('/psicologo/Contatos') }} />
+                {/* <Button icon={createIcon("pi pi-users")} className="p-button-outlined p-button-lg p-mr-3 p-col" style={{ height: '4.00em', width: 'auto' }} label='Discentes' onClick={() => { history.push('/profissionalDeSaude/ListaDiscentes') }} /> */}
+                <Button icon={createIcon("pi pi-file")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Parescer psicológico' onClick={() => { history.push('/psicologo/parecereres') }} />
+                {/* <Button icon={createIcon("pi pi-bell")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Reportes' onClick={() => { history.push('/profissionalDeSaude/ListaDeReportes') }} /> */}
+                <Button icon={createIcon("pi pi-chart-line")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Estatísticas' onClick={() => { history.push('/psicologo/consultarEstatisticas') }}/>
+                <Button icon={createIcon("pi pi-user")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Psicólogo' onClick={() => { history.push('/psicologo/perfil') }} />
+            </div>
+        </React.Fragment>
+    );
+
+    const leftContentsMobile = (
+        <React.Fragment>
+            <div className='' style={{ padding: '1em' }}>
+
+                <Button icon={createIcon("pi pi-home")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Home' onClick={() => { history.push('/') }} />
+                <Button icon={createIcon("pi pi-box")} className="p-button-outlined p-button-lg  p-mr-3 p-mt-3 p-col" label='Materiais online' onClick={() => { history.push('/publica/MateriaisOnline') }} />
+                {/* <Button icon={createIcon("pi pi-heart-fill")} className="p-button-outlined p-button-lg p-mr-3 p-col" label='Simular Questionario' /> */}
+                <Button icon={createIcon("pi pi-info-circle")} className="p-button-outlined p-button-lg  p-mr-3 p-mt-3 p-col" label='Quem Somos' onClick={() => { history.push('/publica/QuemSomos') }} />
+                <Button icon={createIcon("pi pi-comments")} className="p-button-outlined p-button-lg p-mr-3  p-mt-3 p-col" label='Contatos' onClick={() => { history.push('/psicologo/Contatos') }} />
+                {/* <Button icon={createIcon("pi pi-users")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Discentes' onClick={() => { history.push('/profissionalDeSaude/ListaDiscentes') }} /> */}
+                <Button icon={createIcon("pi pi-file")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Parescer psicológico' onClick={() => { history.push('/psicologo/parecereres') }} />
+                {/* <Button icon={createIcon("pi pi-bell")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Reportes' onClick={() => { history.push('/profissionalDeSaude/ListaDeReportes') }} /> */}
+                <Button icon={createIcon("pi pi-chart-line")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Estatísticas' onClick={() => { history.push('/psicologo/consultarEstatisticas') }}/>
+                <Button icon={createIcon("pi pi-user")} className="p-button-outlined p-button-lg p-mr-3 p-mt-3 p-col" label='Psicólogo' onClick={() => { history.push('/psicologo/perfil') }} />
+                <FotoPerfil></FotoPerfil>
+                <NomePerfil></NomePerfil>
+            </div>
+        </React.Fragment>
+    );
+
+    const rightContentsMobile = (
+        <React.Fragment>
+            <div style={{}} className='p-col-1 p-mr-3'  >
+                <img src={logoImg} alt="logo" style={{ height: '10.75em', width: 'auto' }} />
+            </div>
+        </React.Fragment>
+    );
+
+    if (largura < 640) {
+        return (
+
+            <div className=''>
+                <Toolbar right={leftContentsMobile} left={rightContentsMobile} />
+            </div>
+
+        );
+    }
+    else {
+        return (
+
+            <div className='p-flex'>
+                <Toolbar left={leftContents} right={rightContents} />
+            </div>
+
+        );
+    }
+
+}
