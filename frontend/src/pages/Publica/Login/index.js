2fc0c15bd9648262d68845ccf8fbd188333673a2
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-02-19 00:31:34 -0300

    Migração para a nova biblioteca de serviços de identidade do Google
    A antiga biblioteca foi substituída pela nova no componente Login.

--- frontend/src/pages/Publica/Login/index.js
+++ frontend/src/pages/Publica/Login/index.js
@@ -1,13 +1,13 @@
 import { Card } from 'primereact/card';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './styles.css'
 import { Button } from 'primereact/button';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import ToobarPublica from '../ToobarPublica';
-import GoogleLogin from 'react-google-login';
 import { Toast } from 'primereact/toast';
 import axios from 'axios';
 import URL from '../../../services/URL';
+import jwt_decode from "jwt-decode";
 
 export default function Login() {
 
@@ -23,9 +23,33 @@ export default function Login() {
     const history = useHistory();
     const [permissionProfSaude, setPermissionProfSaude] = useState(false);
     const [permissionPsicologo, setpermissionPsicologo] = useState(false);
-    const responseGoogle = (response) => {
 
-        const { profileObj: { name, email, imageUrl, googleId } } = response;
+    useEffect(async () => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "1074576164125-iqmvj2ij72e3evgu6gaenjhspj3n5v46.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        })
+        google.accounts.id.renderButton(
+            document.getElementById("singnInDiv"),
+            {
+                theme: "outline",
+                size: "large"
+            }
+        )
+    }, []);
+
+    function handleCallbackResponse(response) {
+
+        if (response == null || response == undefined){
+            errorLogin();
+        }
+        var token = response.credential;
+        var decoded = jwt_decode(token);
+        const name = decoded.name;
+        const email = decoded.email;
+        const imageUrl = decoded.picture;
+        const googleId = decoded.sub;
         const usuario =
         {
             "nome": name,
@@ -181,7 +205,7 @@ export default function Login() {
                 title="AUTENTIQUE-SE!"
                 subTitle={<div><b>Estudante ou profissional de saúde</b>, para ter acesso a todas as opções da plataforma HelpMind você deve entrar com a sua conta do IFPB.</div>} >
                 <div className="login-container p-d-flex p-grid p-flex-wrap p-justify-center p-align-center p-mt-6" style={{ height: '100%' }} >
-                    <div className="p-col-4 p-component">
+                    <div className="p-col-5 p-component">
                         <ul>
                             <li>Inventário de Depressão de Beck (BDI)</li>
                             <li>Inventário de Ansiedade de Beck (BAI)</li>
@@ -190,14 +214,8 @@ export default function Login() {
                         </ul>
                     </div>
                     <div className="=p-col-9 p-flex p-grid p-flex-column p-p-3">
-                        <GoogleLogin
-                            clientId="1074576164125-iqmvj2ij72e3evgu6gaenjhspj3n5v46.apps.googleusercontent.com"
-                            buttonText={<div><b>ENTRAR COM E-MAIL</b> @ifpb.edu.br ou @academico.ifpb.edu.br</div>}
-                            onSuccess={responseGoogle}
-                            onFailure={errorLogin}
-                        // cookiePolicy={'single_host_origin'}
-                        ></GoogleLogin>
-                        <Button className="p-mt-4" label={<div><b>NÃO POSSUO E-MAIL</b><br />ACADÊMICO OU INSTITUCIONAL</div>} onClick={infoEmail} />
+                        <div className="p-mb-5 p-component" id="singnInDiv"></div>
+                        <Button className="p-mt-4 p-mb-6" label={<div><b>NÃO POSSUO E-MAIL</b><br />ACADÊMICO OU INSTITUCIONAL</div>} onClick={infoEmail} />
                     </div>
                 </div>
             </Card>
