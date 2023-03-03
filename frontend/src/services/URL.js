1ed3eec5db0f0996a84afc18e4c3022b1b6c9c9d
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-03-03 15:39:20 -0300

    Configuração para rodar no servidor
    SSL, URI e Admin

--- frontend/src/services/URL.js
+++ frontend/src/services/URL.js
@@ -1,5 +1,5 @@
-const DOMINIO = 'http://localhost:8080';
-// const DOMINIO = 'https://adsmt.ifpb.edu.br:3101';
+//const DOMINIO = 'http://localhost:8080';
+const DOMINIO = 'https://adsmt.ifpb.edu.br:3101';
 
 class URL {
 
