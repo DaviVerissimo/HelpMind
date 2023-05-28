80feae8a8d0d4bd46123528b2a908612f555274d
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-05-28 12:21:24 -0300

    algumas classes dto
    Envelope encapsula uma lista de mensagens e o ID da sua respectiva comversa. Identificadores encapsula idProfSaude, idPsicologo e idDiscente.

---
+++ backend/HelpMind/src/main/java/com/helpmind/controller/Identificadores.java
@@ -0,0 +1,27 @@
+package com.helpmind.controller;
+
+public class Identificadores {
+	
+	private String idDiscente;
+	private String idProfissionalSaude;
+	private String idPsicologo;
+	public String getIdDiscente() {
+		return idDiscente;
+	}
+	public void setIdDiscente(String idDiscente) {
+		this.idDiscente = idDiscente;
+	}
+	public String getIdProfissionalSaude() {
+		return idProfissionalSaude;
+	}
+	public void setIdProfissionalSaude(String idProfissionalSaude) {
+		this.idProfissionalSaude = idProfissionalSaude;
+	}
+	public String getIdPsicologo() {
+		return idPsicologo;
+	}
+	public void setIdPsicologo(String idPsicologo) {
+		this.idPsicologo = idPsicologo;
+	}
+
+}
