01fc7519870b347b4660c5ab76fc8a7987b90d8b
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-07-23 17:17:21 -0300

    Adição de  componente NovoProntuarioProfSaude

---
+++ frontend/src/pages/ProfissionalDeSaude/NovoProntuarioProfSaude/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToobarProfissionalDeSaude from '../ToobarProfissionalDeSaude';
+import NovoProntuarioComponente from '../../../Components/NovoProntuarioComponente';
+
+export default function NovoProntuarioProfSaude() {
+
+    return (
+        <div>
+            <ToobarProfissionalDeSaude></ToobarProfissionalDeSaude>
+            <NovoProntuarioComponente></NovoProntuarioComponente>
+        </div>
+
+    );
+}
