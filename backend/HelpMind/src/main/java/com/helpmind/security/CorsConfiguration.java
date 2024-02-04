f69cd7b6917718642a1f9790116de9b86e6f6fd1
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-02-04 17:38:13 -0300

    Update CorsConfiguration.java

--- backend/HelpMind/src/main/java/com/helpmind/security/CorsConfiguration.java
+++ backend/HelpMind/src/main/java/com/helpmind/security/CorsConfiguration.java
@@ -1,16 +1,22 @@
 package com.helpmind.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+	
+	@Value("${config.frontend.uri}")
+	private String URI;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+            .allowedOrigins(URI)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
+            .allowCredentials(true)
+            .maxAge(3600); 
     }
 }
