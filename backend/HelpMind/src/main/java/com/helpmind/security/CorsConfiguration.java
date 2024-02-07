2633dfcc0904c1ea605d5e788beb0924a68b3421
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-02-07 07:20:41 -0300

    Update CorsConfiguration.java

--- backend/HelpMind/src/main/java/com/helpmind/security/CorsConfiguration.java
+++ backend/HelpMind/src/main/java/com/helpmind/security/CorsConfiguration.java
@@ -16,6 +16,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
             .allowedOrigins(URI)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
+            .allowedHeaders("Authorization", "Content-Type")
+            .exposedHeaders("Authorization")
             .allowCredentials(true)
             .maxAge(3600); 
     }
