2e5dba19e929fcb1ec3d11ea02b261eed24a5ced
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-02-07 07:17:28 -0300

    Update URL.js

--- frontend/src/services/URL.js
+++ frontend/src/services/URL.js
@@ -1,10 +1,21 @@
 const DOMINIO = 'http://localhost:8080';
 //const DOMINIO = 'https://adsmt.ifpb.edu.br:3101';
 
+const CLIENT_ID = '';
+const REDIRECT_URI  = '';
+
 class URL {
 
     getDominio() {
         return DOMINIO;
+    }
+
+    getClientId() {
+        return CLIENT_ID;
+    }
+
+    getRedirectUri() {
+        return REDIRECT_URI;
     }
 
 }
